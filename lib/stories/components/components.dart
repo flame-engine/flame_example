fb7367a7494d21bffc9ff72a4a3a57a454d3c6e4
Erick Zanardo <erickzanardoo@gmail.com>
2021-01-08 13:51:47 -0300

    Adding code link for examples

--- lib/stories/components/components.dart
+++ lib/stories/components/components.dart
@@ -1,11 +1,21 @@
 import 'package:dashbook/dashbook.dart';
-import 'package:flame/game/game_widget.dart';
 
 import './composability.dart';
 import './debug.dart';
+import '../../commons/dashbook_gamewidget.dart';
 
 void addComponentsStories(Dashbook dashbook) {
   dashbook.storiesOf('Components')
-    ..add('composability', (_) => GameWidget(game: Composability()))
-    ..add('debug', (_) => GameWidget(game: Debug()));
+    ..add(
+      'composability',
+      (_) => DashbookGameWidget(game: Composability()),
+      codeLink:
+          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/components/composability.dart',
+    )
+    ..add(
+      'debug',
+      (_) => DashbookGameWidget(game: Debug()),
+      codeLink:
+          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/components/debug.dart',
+    );
 }
