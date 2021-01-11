3852145246706c772e925f03cad726d9d8771ef8
Erick Zanardo <erickzanardoo@gmail.com>
2021-01-11 09:04:56 -0300

    PR suggestions

--- lib/stories/animations/animations.dart
+++ lib/stories/animations/animations.dart
@@ -3,19 +3,18 @@ import 'package:dashbook/dashbook.dart';
 import './basic.dart';
 import './aseprite.dart';
 import '../../commons/dashbook_gamewidget.dart';
+import '../../commons/commons.dart';
 
 void addAnimationStories(Dashbook dashbook) {
   dashbook.storiesOf('Animations')
     ..add(
       'basic animations',
       (_) => DashbookGameWidget(game: BasicAnimations()),
-      codeLink:
-          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/animations/basic.dart',
+      codeLink: baseLink('animations/basic.dart'),
     )
     ..add(
       'aseprite',
       (_) => DashbookGameWidget(game: Aseprite()),
-      codeLink:
-          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/animations/aseprite.dart',
+      codeLink: baseLink('animations/aseprite.dart'),
     );
 }
--- lib/stories/components/components.dart
+++ lib/stories/components/components.dart
@@ -3,19 +3,18 @@ import 'package:dashbook/dashbook.dart';
 import './composability.dart';
 import './debug.dart';
 import '../../commons/dashbook_gamewidget.dart';
+import '../../commons/commons.dart';
 
 void addComponentsStories(Dashbook dashbook) {
   dashbook.storiesOf('Components')
     ..add(
       'composability',
       (_) => DashbookGameWidget(game: Composability()),
-      codeLink:
-          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/components/composability.dart',
+      codeLink: baseLink('components/composability.dart'),
     )
     ..add(
       'debug',
       (_) => DashbookGameWidget(game: Debug()),
-      codeLink:
-          'https://github.com/flame-engine/flame_example/blob/master/lib/stories/components/debug.dart',
+      codeLink: baseLink('components/debug.dart'),
     );
 }
