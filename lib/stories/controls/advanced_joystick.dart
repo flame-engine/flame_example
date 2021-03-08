b663df21adb44b8605971dbe8cd12e1fc3bb12fa
Luan Nico <luanpotter27@gmail.com>
2021-03-08 11:30:12 -0500

    Backport joystick example and add advanced version (#17)

--- lib/stories/controls/advanced_joystick.dart
+++ lib/stories/controls/advanced_joystick.dart
@@ -1,26 +1,39 @@
 import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flame/joystick.dart';
-import 'package:flame/extensions.dart';
 import 'package:flutter/material.dart';
 
 import 'joystick_player.dart';
 
-class JoystickGame extends BaseGame with HasDraggableComponents {
+class AdvancedJoystickGame extends BaseGame with HasDraggableComponents {
+  Future<Sprite> loadJoystick(int idx) async {
+    return loadSprite(
+      'joystick.png',
+      srcPosition: Vector2(idx * 16.0, 0),
+      srcSize: Vector2.all(16),
+    );
+  }
+
   @override
   Future<void> onLoad() async {
     final joystick = JoystickComponent(
       gameRef: this,
-      directional: JoystickDirectional(),
+      directional: JoystickDirectional(
+        background: JoystickElement.sprite(await loadJoystick(0)),
+        knob: JoystickElement.sprite(await loadJoystick(1)),
+      ),
       actions: [
         JoystickAction(
           actionId: 1,
           margin: const EdgeInsets.all(50),
-          color: const Color(0xFF0000FF),
+          action: JoystickElement.sprite(await loadJoystick(2)),
+          actionPressed: JoystickElement.sprite(await loadJoystick(4)),
         ),
         JoystickAction(
           actionId: 2,
-          color: const Color(0xFF00FF00),
+          action: JoystickElement.sprite(await loadJoystick(3)),
+          actionPressed: JoystickElement.sprite(await loadJoystick(5)),
           margin: const EdgeInsets.only(
             right: 50,
             bottom: 120,
@@ -30,6 +43,9 @@ class JoystickGame extends BaseGame with HasDraggableComponents {
           actionId: 3,
           margin: const EdgeInsets.only(bottom: 50, right: 120),
           enableDirection: true,
+          color: const Color(0xFFFF00FF),
+          opacityBackground: 0.1,
+          opacityKnob: 0.9,
         ),
       ],
     );
